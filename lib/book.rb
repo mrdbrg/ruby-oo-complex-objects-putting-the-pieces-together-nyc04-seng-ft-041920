e020ff8875f538f5ef9d4f5f2212a74911bcf7db
Marlon Braga <rdg.brg@gmail.com>
2020-05-12 18:32:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  attr_accessors :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+end
+
+book = Book.new("Some title")
+book.turn_page
